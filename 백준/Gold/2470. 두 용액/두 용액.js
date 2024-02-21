17a0aa392ee72d31706a5b8d6a86ce46062ec34d
baegyeong <102566546+baegyeong@users.noreply.github.com>
2024-02-22 04:28:13 +0900

    [Gold V] Title: 두 용액, Time: 228 ms, Memory: 22464 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+const n = Number(input[0]);
+const arr = input[1]
+  .split(" ")
+  .map(Number)
+  .sort((a, b) => a - b);
+
+let start = 0;
+let end = n - 1;
+
+let max = Infinity;
+let a = 0,
+  b = 0;
+while (start < end) {
+  const answer = arr[start] + arr[end];
+  if (Math.abs(answer) < Math.abs(max)) {
+    max = answer;
+    a = arr[start];
+    b = arr[end];
+  }
+  if (answer === 0) {
+    break;
+  } else if (answer < 0) {
+    start++;
+  } else {
+    end--;
+  }
+}
+
+console.log(a, b);
