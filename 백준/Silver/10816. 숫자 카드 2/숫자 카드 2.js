c2dcc5c20801d493bf78fd2f5c18d3e3220a016e
baegyeong <102566546+baegyeong@users.noreply.github.com>
2024-05-13 21:32:46 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1280 ms, Memory: 150768 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.js
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.js
@@ -1,40 +1,34 @@
 const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().split("\n");
 
-const n = Number(input[0]);
-const m = Number(input[2]);
+const N = Number(input[0]);
+const n_arr = input[1]
+  .split(" ")
+  .map(Number)
+  .sort((a, b) => a - b);
 
-let arr1 = input[1].split(" ").map(Number);
-let arr2 = input[3].split(" ").map(Number);
-arr1.sort((a, b) => a - b);
+let m_arr = input[3].split(" ").map(Number);
 
-function lowerBound(arr, target, start, end) {
+const lower_bound = (target, len) => {
+  let start = 0;
+  let end = len;
   while (start < end) {
     let mid = parseInt((start + end) / 2);
-    if (arr1[mid] >= target) end = mid;
+    if (n_arr[mid] >= target) end = mid;
     else start = mid + 1;
   }
-  return end;
-}
+  return start;
+};
 
-function upperBound(arr, target, start, end) {
+const upper_bound = (target, len) => {
+  let start = 0;
+  let end = len;
   while (start < end) {
     let mid = parseInt((start + end) / 2);
-    if (arr1[mid] > target) end = mid;
+    if (n_arr[mid] > target) end = mid;
     else start = mid + 1;
   }
-  return end;
-}
+  return start;
+};
 
-function countByRange(arr, leftValue, rightValue) {
-  let rightIndex = upperBound(arr, rightValue, 0, arr.length);
-  let leftIndex = lowerBound(arr, leftValue, 0, arr.length);
-  return rightIndex - leftIndex;
-}
-
-let result = "";
-for (let i = 0; i < m; i++) {
-  result += countByRange(arr1, arr2[i], arr2[i]) + " ";
-}
-
-console.log(result);
+console.log(m_arr.map((x) => upper_bound(x, N) - lower_bound(x, N)).join(" "));
