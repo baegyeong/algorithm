9c91c4be06a5ab0b78e21a5d1ae4c1dd7831c58f
baegyeong <102566546+baegyeong@users.noreply.github.com>
2025-02-07 20:51:27 +0900

    [Silver IV] Title: 설탕 배달, Time: 100 ms, Memory: 9344 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.js
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.js
@@ -1,21 +1,20 @@
 const fs = require("fs");
-const input = fs.readFileSync("/dev/stdin").toString().split("\n");
+let input = +fs.readFileSync("/dev/stdin").toString().split("\n")[0];
 
-let n = Number(input[0]);
-let cnt = 0;
-let flag = false;
-
-while (n >= 0) {
-  if (n === 0 || n % 5 === 0) {
-    cnt += parseInt(n / 5);
-    console.log(cnt);
-    flag = true;
+let result = 0;
+while (input > 0) {
+  if (input % 5 === 0) {
+    input -= 5;
+  } else if (input % 3 === 0) {
+    input -= 3;
+  } else if (input >= 5) {
+    input -= 5;
+  } else if (input >= 3) {
+    input -= 3;
+  } else {
     break;
   }
-  n -= 3;
-  cnt += 1;
+  result++;
 }
 
-if (!flag) {
-  console.log(-1);
-}
+console.log(input === 0 ? result : -1);
