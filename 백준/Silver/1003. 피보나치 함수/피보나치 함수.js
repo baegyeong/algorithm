5b369d354c405c745fce97841921e08c0c36253e
baegyeong <102566546+baegyeong@users.noreply.github.com>
2025-10-03 18:37:17 +0900

    [Silver III] Title: 피보나치 함수, Time: 88 ms, Memory: 9616 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.js
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.js
@@ -1,18 +1,25 @@
 const fs = require("fs");
-const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const [T, ...input] = fs
+  .readFileSync("/dev/stdin")
+  .toString().trim()
+  .split("\n")
+  .map(Number);
 
-d = new Array(100).fill(0);
-d[0] = 0;
-d[1] = 1;
+const dp = Array.from({ length: 41 }, () => Array(2));
 
-for (let i = 2; i <= 40; i++) {
-  d[i] = d[i - 1] + d[i - 2];
+dp[0] = [1, 0];
+dp[1] = [0, 1];
+
+const maxN = Math.max(...input);
+
+for (let i = 2; i <= maxN; i++) {
+  dp[i][0] = dp[i - 1][0] + dp[i - 2][0];
+  dp[i][1] = dp[i - 1][1] + dp[i - 2][1];
 }
 
-let num = Number(input[0]);
-for (let i = 1; i <= num; i++) {
-  let n = Number(input[i]);
-
-  if (n === 0) console.log(1, 0);
-  else console.log(d[n - 1], d[n]);
+let answer = "";
+for (const x of input) {
+  answer += `${dp[x][0]} ${dp[x][1]}\n`;
 }
+
+console.log(answer);
