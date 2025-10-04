ce876e87afd95e9608ee60b5c16ae016a4c1da4e
baegyeong <102566546+baegyeong@users.noreply.github.com>
2025-10-04 18:08:55 +0900

    [Silver III] Title: 이친수, Time: 92 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const N = +fs.readFileSync("/dev/stdin").toString().trim();
+
+const dp = new Array(N + 1).fill(0);
+dp[1] = 1;
+dp[2] = 1;
+
+for (let i = 3; i <= N; i++) {
+  dp[i] = BigInt(dp[i - 1]) + BigInt(dp[i - 2]);
+}
+
+console.log(String(dp[N]));
