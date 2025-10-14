f034c6779142e5ba48ff3e0f3d832a13a51c021b
baegyeong <102566546+baegyeong@users.noreply.github.com>
2025-10-15 02:40:15 +0900

    [Gold IV] Title: 호숫가의 개미굴, Time: 332 ms, Memory: 57404 KB, Score: 21 point -BaekjoonHub

--- 백준/Gold/28325. 호숫가의 개미굴/호숫가의 개미굴.js
+++ 백준/Gold/28325. 호숫가의 개미굴/호숫가의 개미굴.js
@@ -2,27 +2,40 @@ const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
 const N = +input[0];
-const arr = input[1].split(" ").map(Number);
+const arr = input[1].split(" ").map(BigInt);
 
-const dp = Array.from({ length: N });
+const dp = Array.from({ length: N }, () => 0n);
 
-dp[0] = Math.max(arr[0], 1);
-if (arr[0]) {
-  dp[1] = Math.max(dp[0] + arr[1], dp[0] + 1);
+if (!arr[1] && arr[2]) {
+  dp[0] = arr[0];
 } else {
-  dp[1] = Math.max(dp[0], dp[0] + arr[1]);
+  dp[0] = arr[0] > 1n ? arr[0] : 1n;
+}
+
+if (!arr[0] && dp[0]) {
+  dp[1] = dp[0] + arr[1];
+} else {
+  dp[1] = dp[0] + 1n > dp[0] + arr[1] ? dp[0] + 1n : dp[0] + arr[1];
 }
 
 for (let i = 2; i < N; i++) {
-  if (arr[i - 1]) {
-    dp[i] = Math.max(dp[i - 1] + 1, dp[i - 1] + arr[i]);
+  if (i === N - 1) {
+    if ((dp[i - 1] !== dp[i - 2] && !arr[i - 1]) || (dp[0] && !arr[0])) {
+      dp[i] = dp[i - 1] + arr[i];
+    } else {
+      dp[i] =
+        dp[i - 1] + arr[i] > dp[i - 1] + 1n
+          ? dp[i - 1] + arr[i]
+          : dp[i - 1] + 1n;
+    }
   } else if (arr[i]) {
     dp[i] = dp[i - 1] + arr[i];
-  } else if (i === N - 1 && dp[0]) {
-    dp[i] = dp[i - 1] + arr[i];
+  } else if (arr[i - 1]) {
+    dp[i] =
+      dp[i - 1] + 1n > dp[i - 1] + arr[i] ? dp[i - 1] + 1n : dp[i - 1] + arr[i];
   } else {
-    dp[i] = Math.max(dp[i - 2] + 1, dp[i - 1]);
+    dp[i] = dp[i - 2] + 1n > dp[i - 1] ? dp[i - 2] + 1n : dp[i - 1];
   }
 }
 
-console.log(dp[N - 1]);
+console.log(dp[N - 1].toString());
