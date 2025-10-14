aaf49ed7c83cb4183b0d5ef5b04597cf516a63d4
baegyeong <102566546+baegyeong@users.noreply.github.com>
2025-10-15 03:14:31 +0900

    [Gold IV] Title: 호숫가의 개미굴, Time: 356 ms, Memory: 67552 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/28325. 호숫가의 개미굴/호숫가의 개미굴.js
+++ 백준/Gold/28325. 호숫가의 개미굴/호숫가의 개미굴.js
@@ -1,30 +1,32 @@
 const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
-const N = +input[0];
-let arr = input[1].split(" ").map(Number);
+const N = BigInt(input[0]);
+let arr = input[1].split(" ").map(BigInt);
 
-if (arr.every((item) => item === 0)) {
-  console.log(Math.floor(N / 2));
+if (arr.every((item) => item === 0n)) {
+  console.log((N / 2n).toString());
   return;
 }
 
-let total = arr.reduce((v, i) => v + i, 0);
-const start = arr.findIndex((item) => item === 1);
+let total = arr.reduce((v, i) => v + i, 0n);
+const start = arr.findIndex((item) => item === 1n);
 arr = arr.slice(start + 1).concat(arr.slice(0, start + 1));
 
-const dp = Array({ length: N }).fill(0);
+const dp = Array(Number(N)).fill(0n);
 
-for (let i = 0; i < N; i++) {
-  if (arr[i] || dp[i]) continue;
+for (let i = 0n; i < N; i++) {
+  const idx = Number(i);
+  if (arr[idx] || dp[idx]) continue;
 
   let j = i;
   for (; j < N; j++) {
-    if (arr[j]) break;
-    dp[j] = 1;
+    const jdx = Number(j);
+    if (arr[jdx]) break;
+    dp[jdx] = 1n;
   }
 
-  total += Math.floor((j - i + 1) / 2);
+  total += (j - i + 1n) / 2n;
 }
 
-console.log(total);
+console.log(total.toString());
