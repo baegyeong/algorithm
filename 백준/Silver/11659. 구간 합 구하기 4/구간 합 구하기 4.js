d89836e3401c7357eb1d13c494ec9356f2763272
baegyeong <102566546+baegyeong@users.noreply.github.com>
2025-11-17 11:48:29 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 344 ms, Memory: 56236 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.js
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.js
@@ -1,16 +1,21 @@
-const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
-const arr = input[1].split(' ').map(v => +v);
-const cumsum = new Array(arr.length+1).fill(0);
-const output = [];
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
-arr.forEach((v, i) => {
-  cumsum[i+1] = cumsum[i] + v;
-});
+const [N, M] = input[0].split(" ").map(Number);
+const arr = input[1].split(" ").map(Number);
+const range = input.slice(2).map((item) => item.split(" ").map(Number));
 
+const answer = [];
+const dp = new Array(N + 1).fill(0);
+dp[0] = 0;
+dp[1] = arr[0];
 
-input.slice(2).forEach(ij => {
-  const [i, j] = ij.split(' ').map(v => +v);
-  output.push(cumsum[j]-cumsum[i-1]);
-});
+for (let i = 2; i <= N; i++) {
+  dp[i] += dp[i - 1] + arr[i - 1];
+}
 
-console.log(output.join('\n'));
+for (const [i, j] of range) {
+  answer.push(dp[j] - dp[i - 1]);
+}
+
+console.log(answer.join("\n"));
