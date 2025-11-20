76c30ccd7c55eb1210587f376f3a41b51d167ae6
baegyeong <102566546+baegyeong@users.noreply.github.com>
2025-11-20 10:38:45 +0900

    [Silver II] Title: 연속합, Time: 168 ms, Memory: 15752 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.js
+++ 백준/Silver/1912. 연속합/연속합.js
@@ -1,12 +1,13 @@
 const fs = require("fs");
-const input = fs.readFileSync("/dev/stdin").toString().split("\n");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
-const num = Number(input[0]);
+const n = +input[0];
 const arr = input[1].split(" ").map(Number);
-let d = [arr[0]];
+const dp = new Array(n).fill(0);
+dp[0] = arr[0];
 
-for (let i = 1; i < num; i++) {
-  d[i] = arr[i] > arr[i] + d[i - 1] ? arr[i] : arr[i] + d[i - 1];
+for (let i = 1; i < n; i++) {
+  dp[i] = Math.max(arr[i], dp[i - 1] + arr[i]);
 }
 
-console.log(Math.max(...d));
+console.log(Math.max(...dp));
