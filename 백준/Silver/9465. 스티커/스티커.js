82269bbd656082e4f201a9064447bc81f5baa611
baegyeong <102566546+baegyeong@users.noreply.github.com>
2025-11-20 12:01:29 +0900

    [Silver I] Title: 스티커, Time: 648 ms, Memory: 123348 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.js
+++ 백준/Silver/9465. 스티커/스티커.js
@@ -1,18 +1,23 @@
-var fs = require("fs");
-var inputs = fs.readFileSync("/dev/stdin").toString().split("\n");
-var cases = Number(inputs[0]);
-inputs.shift();
-for (var num = 0; num < cases; num++) {
-  var count = Number(inputs[3 * num]);
-  var up = inputs[3 * num + 1].split(" ").map((v) => Number(v));
-  var down = inputs[3 * num + 2].split(" ").map((v) => Number(v));
-  var dp = [[0, up[0], down[0]]];
-  for (var i = 1; i < count; i++) {
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+const T = +input[0];
+input.shift();
+
+for (let i = 0; i < T; i++) {
+  const n = +input[3 * i];
+  const up = input[3 * i + 1].split(" ").map(Number);
+  const down = input[3 * i + 2].split(" ").map(Number);
+
+  const dp = [[0, up[0], down[0]]];
+
+  for (let i = 1; i < n; i++) {
     dp[i] = [
       Math.max(...dp[i - 1]),
       Math.max(dp[i - 1][0], dp[i - 1][2]) + up[i],
       Math.max(dp[i - 1][0], dp[i - 1][1]) + down[i],
     ];
   }
-  console.log(Math.max(...dp[count - 1]));
+
+  console.log(Math.max(...dp[n - 1]));
 }
