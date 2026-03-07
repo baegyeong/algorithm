5a2cd97cf7b7ac5b2816631fa8a940d0534c3b66
baegyeong <102566546+baegyeong@users.noreply.github.com>
2026-03-07 17:57:30 +0900

    [Silver I] Title: 봄버맨, Time: 340 ms, Memory: 19360 KB -BaekjoonHub

---
+++ 백준/Silver/16918. 봄버맨/봄버맨.js
@@ -0,0 +1,64 @@
+const isLocal = process.platform !== "linux";
+const filePath = isLocal ? "input.txt" : "/dev/stdin";
+
+const input = require("fs")
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n");
+
+const [R, C, N] = input[0].split(" ").map(Number);
+let board = input.slice(1).map((line) => line.split(""));
+let allBomb = Array.from({ length: R }, () => Array(C).fill("O"));
+
+const state = Array.from({ length: R }, () => Array(C).fill(-1));
+
+for (let i = 0; i < R; i++) {
+  for (let j = 0; j < C; j++) {
+    if (board[i][j] === "O") {
+      state[i][j] = 0;
+    }
+  }
+}
+
+const directions = [
+  [0, 1],
+  [1, 0],
+  [-1, 0],
+  [0, -1],
+];
+
+function bomb() {
+  for (let x = 0; x < R; x++) {
+    for (let y = 0; y < C; y++) {
+      if (board[x][y] === ".") continue;
+
+      allBomb[x][y] = ".";
+
+      for (const [dx, dy] of directions) {
+        const nx = dx + x;
+        const ny = dy + y;
+
+        if (nx < 0 || nx >= R || ny < 0 || ny >= C) continue;
+        allBomb[nx][ny] = ".";
+      }
+    }
+  }
+
+  board = [...allBomb];
+  allBomb = Array.from({ length: R }, () => Array(C).fill("O"));
+}
+
+function printBoard(board) {
+  console.log(board.map((item) => item.join("")).join("\n"));
+}
+
+for (let i = 3; i <= N; i += 2) {
+  bomb();
+}
+
+if (N % 2 === 0) {
+  printBoard(allBomb);
+} else {
+  printBoard(board);
+}
