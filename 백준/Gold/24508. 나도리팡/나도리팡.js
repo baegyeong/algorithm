66692f6b9ae9ec9ea2c3675f31953ed4d6488078
baegyeong <102566546+baegyeong@users.noreply.github.com>
2026-03-08 15:07:21 +0900

    [Gold V] Title: 나도리팡, Time: 224 ms, Memory: 20516 KB -BaekjoonHub

--- 백준/Gold/24508. 나도리팡/나도리팡.js
+++ 백준/Gold/24508. 나도리팡/나도리팡.js
@@ -12,24 +12,17 @@ input.sort((a, b) => a - b);
 let left = 0,
   right = N - 1;
 
-if (T === 0 && input.every((item) => item === 0)) {
-  console.log("YES");
-  return;
-}
-
-while (T > 0 && left < right) {
+while (left < right) {
   const diff = Math.min(input[left], K - input[right]);
-  input[right] += diff;
-
-  input[left] -= diff;
   T -= diff;
 
-  if (input[left] === 0) {
-    left++;
-  }
+  if (T < 0) break;
 
-  if (left === right) break;
+  input[right] += diff;
+  input[left] -= diff;
+
+  if (input[left] === 0) left++;
   if (input[right] === K) right--;
 }
 
-console.log(input[right] === K && T >= 0 ? "YES" : "NO");
+console.log(input[left] === K || (input[left] === 0 && T >= 0) ? "YES" : "NO");
