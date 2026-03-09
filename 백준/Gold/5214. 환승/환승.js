4aeee07c11641bc2d594e37714cd4a64a63cb4fe
baegyeong <102566546+baegyeong@users.noreply.github.com>
2026-03-09 10:06:21 +0900

    [Gold II] Title: 환승, Time: 6260 ms, Memory: 92452 KB -BaekjoonHub

---
+++ 백준/Gold/5214. 환승/환승.js
@@ -0,0 +1,43 @@
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+
+const [[N, K, M], ...input] = require("fs")
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n")
+  .map((line) => line.split(" ").map(Number));
+
+const hypertube = Array.from({ length: N + 1 }, () => []);
+const visited = Array(N + 1).fill(false);
+
+for (let i = 0; i < M; i++) {
+  input[i] = input[i].sort((a, b) => b - a);
+  for (const station of input[i]) {
+    hypertube[station].push(i);
+  }
+}
+
+const queue = [[1, 1]];
+visited[1] = true;
+
+let head = 0;
+
+while (head < queue.length) {
+  const [station, count] = queue[head++];
+
+  if (station === N) {
+    console.log(count);
+    return;
+  }
+
+  for (const hyper of hypertube[station]) {
+    for (const next of input[hyper]) {
+      if (visited[next]) continue;
+
+      visited[next] = true;
+      queue.push([next, count + 1]);
+    }
+  }
+}
+
+console.log(-1);
