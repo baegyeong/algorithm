d11c1689c09301876f80674be6ffe0b3eb9f2936
baegyeong <102566546+baegyeong@users.noreply.github.com>
2026-03-26 16:33:29 +0900

    [Gold IV] Title: 가로등, Time: 856 ms, Memory: 231232 KB, Score: 40 point -BaekjoonHub

---
+++ 백준/Gold/32069. 가로등/가로등.js
@@ -0,0 +1,39 @@
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+
+const [[L, N, K], input] = require("fs")
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n")
+  .map((line) => line.split(" ").map(Number));
+
+const queue = [];
+const result = [];
+const visited = new Set();
+let head = 0;
+let remaining = K;
+
+for (const x of input) {
+  queue.push([x, 0]);
+  visited.add(x);
+}
+
+while (head < queue.length) {
+  const [target, dist] = queue[head++];
+
+  result.push(dist);
+  remaining--;
+
+  if (remaining === 0) break;
+
+  for (const dx of [-1, 1]) {
+    const nx = target + dx;
+
+    if (nx >= 0 && nx <= L && !visited.has(nx)) {
+      visited.add(nx);
+      queue.push([nx, dist + 1]);
+    }
+  }
+}
+
+console.log(result.join("\n"));
