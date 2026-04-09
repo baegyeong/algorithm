f74488dbd49ae3eeb97de55cc050e71e6248ce1f
baegyeong <102566546+baegyeong@users.noreply.github.com>
2026-04-09 17:00:10 +0900

    [Gold IV] Title: 민서의 응급 수술, Time: 500 ms, Memory: 85272 KB -BaekjoonHub

---
+++ 백준/Gold/20955. 민서의 응급 수술/민서의 응급 수술.js
@@ -0,0 +1,55 @@
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+
+const [[N, M], ...input] = require("fs")
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n")
+  .map((line) => line.split(" ").map(Number));
+
+const graph = Array.from({ length: N + 1 }, () => []);
+
+for (const [u, v] of input) {
+  graph[u].push(v);
+  graph[v].push(u);
+}
+
+const parent = Array.from({ length: N + 1 }, (_, i) => i);
+
+function find(x) {
+  if (parent[x] === x) {
+    return parent[x];
+  }
+
+  parent[x] = find(parent[x]);
+  return parent[x];
+}
+
+function union(a, b) {
+  const rootA = find(a);
+  const rootB = find(b);
+
+  if (rootA < rootB) {
+    parent[rootB] = parent[rootA];
+  } else {
+    parent[rootA] = parent[rootB];
+  }
+}
+
+let cycle = 0;
+
+for (const [u, v] of input) {
+  if (find(u) !== find(v)) {
+    union(u, v);
+  } else {
+    cycle++;
+  }
+}
+
+const root = new Set();
+
+for (let i = 1; i <= N; i++) {
+  root.add(find(i));
+}
+
+console.log(cycle + root.size - 1);
