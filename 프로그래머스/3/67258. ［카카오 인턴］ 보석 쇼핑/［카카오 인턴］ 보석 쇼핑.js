c4b0a161b53608af4c545953d9a34f0b4421a92f
baegyeong <102566546+baegyeong@users.noreply.github.com>
2026-07-05 01:52:43 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 19.54 ms, Memory: 55.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.js
@@ -0,0 +1,41 @@
+function solution(gems) {
+    const gemsSet = new Set(gems)
+    const size = gemsSet.size
+    
+    const gemsCount = {}
+    
+    for(const gem of gemsSet){
+        gemsCount[gem] = 0
+    }
+    
+    let j = 0
+    let answer = []
+    let minDistance = Infinity
+    
+    let jewelryCount = 0
+    
+    for(let i=0; i<=gems.length-size; i++){    
+        while(jewelryCount < size && j < gems.length){
+            if(gemsCount[gems[j]] === 0){
+                jewelryCount++
+            }
+            gemsCount[gems[j]] += 1
+            j++
+        }
+        
+        if(jewelryCount < size && j === gems.length) break;
+
+        const distance = j - i
+        if(minDistance > distance){
+            minDistance = distance
+            answer = [i+1, j]
+        }
+
+        gemsCount[gems[i]] -= 1
+        if(gemsCount[gems[i]] === 0){
+            jewelryCount--
+        }
+    }
+    
+    return answer
+}
