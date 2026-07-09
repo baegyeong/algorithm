bf862825bf3fef028891446ad1048824eaad2966
baegyeong <102566546+baegyeong@users.noreply.github.com>
2026-07-09 12:17:04 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.10 ms, Memory: 44.2 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -1,34 +1,12 @@
 function solution(n, m) {
-    function isPrime(a,b){
-        return a % b === 0
+    function gcd(a, b){
+        if(b === 0) return a
+        return gcd(b, a % b)
     }
     
-    const n_max = []
-    for(let i=1; i<=n; i++){
-        if(isPrime(n,i)) n_max.push(i)
+    function lcm(a, b){
+        return a*b / gcd(a, b)
     }
     
-    const m_max = []
-    for(let i=1; i<=m; i++){
-        if(isPrime(m,i)) m_max.push(i)
-    }
-
-    const result = []
-    for(let i=n_max.length-1; i>=0; i--){
-        if(m_max.includes(n_max[i])) {
-            result.push(n_max[i])
-            break
-        }
-    }
-    
-    let j = Math.max(n, m);    
-    while(true){
-        if(j%n === 0 && j%m===0){
-            result.push(j)
-            break
-        }
-        j++
-    }
-    
-    return result
+    return [gcd(n,m), lcm(n,m)]
 }
