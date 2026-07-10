746e6728cbbfa69abd512699797b7997e11e6222
baegyeong <102566546+baegyeong@users.noreply.github.com>
2026-07-11 02:44:14 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.09 ms, Memory: 43.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.js
@@ -0,0 +1,14 @@
+function solution(n) {
+    let answer = 0
+    
+    while(n>1){
+        if(n%2===0){
+            n/=2
+        }else{
+            n-=1
+            answer++
+        }
+    }
+    
+    return answer+1
+}
