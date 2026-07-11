5c857453dcb9fcf9e642bc2b45b708f8785478f4
baegyeong <102566546+baegyeong@users.noreply.github.com>
2026-07-11 20:44:33 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 58.10 ms, Memory: 69.3 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -1,23 +1,17 @@
 function solution(elements) {
-    const N = elements.length
-    const answer = new Set()
-    
-    for(let i=1; i<=N; i++){
-        let sum = 0
-        let j = 0
-        let count = 0
-        while(j<N+i){
-            let start = j
-            while(count<i){
-                sum += elements[j%N]
-                j++
-                count++
-            }
-            answer.add(sum)
-            sum -= elements[j-i]
-            count--
+    const N = elements.length;
+    const answer = new Set();
+
+    for (let len = 1; len <= N; len++) {
+        let sum = 0;
+        for (let k = 0; k < len; k++) sum += elements[k];
+        answer.add(sum);
+
+        for (let start = 1; start < N; start++) {
+            sum += elements[(start + len - 1) % N] - elements[start - 1];
+            answer.add(sum);
         }
     }
-    
-    return answer.size
+
+    return answer.size;
 }
